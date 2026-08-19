e7a9a8cd9bd298e0fb9aa7695d1d45c1ec72135d
Catarina <kitcatpn@eaportal.org>
2026-08-19 11:25:48 -0300

    frontend adicionado

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 import express, { request, response } from "express"
 import mysql2 from "mysql2"
+import cors from "cors"
 
 const app = express()
 
+app.use(cors())
 app.use(express.json())
 
 app.post("/create-movie", (request, response) => {
