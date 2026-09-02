2acc3b4be1ee6231d183f429d7e0c7c4700c9155
Catarina <kitcatpn@eaportal.org>
2026-09-02 11:22:52 -0300

    arrumei a rota do fetch já que a Catarina não souber arrumar. Ass.: Prof. Marcio Lambisgordo

--- Frontend/script.js
+++ Frontend/script.js
@@ -1,6 +1,6 @@
 async function buscarFilmes() {
     // acessar a rota GET do backend, trazer os filmes e mostrar os filmes na tela
-    const resposta = await fetch("https://filmes-two-beta.vercel.app/") // resposta do backend
+    const resposta = await fetch("https://filmes-two-beta.vercel.app/all-movies") // resposta do backend
     const filmes = await resposta.json() // converte a resposta num objeto JS
     const sectionFilmes = document.querySelector(".filmes")
 
