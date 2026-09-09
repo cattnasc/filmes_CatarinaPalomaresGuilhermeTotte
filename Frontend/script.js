b3f8687823f0e0a9a10547a65a21dce175832461
Catarina <kitcatpn@eaportal.org>
2026-09-09 11:27:26 -0300

    rota delete adicionada

--- Frontend/script.js
+++ Frontend/script.js
@@ -12,9 +12,19 @@ async function buscarFilmes() {
                         <p><strong>Gênero:</strong> ${filme.genero}</p>
                         <p><strong>Duração:</strong> ${filme.duracao} minutos</p>
                         <p><strong>Classificação indicativa:</strong> ${filme.classificacao > 0 ? filme.classificacao + ' anos' : 'Livre'}</p>
+                        <button onclick="apagarFilme(${filme.id})">Apagar</button>
                     </div>
                 `
     })
 }
 
 buscarFilmes()
+
+async function apagarFilme(id) {
+    const respostaDeSucessoAoApagar = await fetch(`https://filmes-backend.vercel.app/delete/${id}`, { method: "DELETE" })
+    const mensagem = await respostaDeSucessoAoApagar.json()
+
+    alert(mensagem.message)
+
+    window.location.reload()
+}
